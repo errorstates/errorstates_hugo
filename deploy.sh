b1081e37dc197b7619dd84cd72ff86ef98f0c18b
Roman K <sharedphysics@gmail.com>
2020-02-15 14:36:16 -0500

    Administrative Updates

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,36 @@
 # If a command fails then the deploy stops
 set -e
 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+# Just a tooltip to let you know the script is running.
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n" 
 
-# Build the project.
-hugo -t amperage # if using a theme, replace with `hugo -t <YOURTHEME>`
+# Build the hugo site to both of your repos
+hugo
+hugo -d ../errorstates.github.io # This builds the public site into your public repo. !!!!! Make sure to replace with your currect public repo here
 
-# Go To Public folder
-cd public
-
-# Add changes to git.
-git add .
+# Deploy your public site
+cd ../errorstates.github.io # Goes to your public site repo
+git init # Initiatilizes git
+git add . # Adds changes
 
 # Commit changes.
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
-git commit -m "$msg"
 
-# Push source and build repos.
-git push origin master
+git commit -m "$msg" # Makes your commit with the message from your deploy script execution
+git push origin master # Pushes the code to GitHub
+
+# Now deploy your backend with all of the 
+cd ../errorstates_hugo #!!!!! Make sure to replace with your currect backend repo here.
+git add . # Adds changes
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+
+git commit -m "$msg" # Makes your commit with the message from your deploy script execution
+git push origin master # Pushes the code to GitHub
